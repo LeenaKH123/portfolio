767c19e9a2035949cbeac449d987cb7ef96cf8ac
leena khalili <engleena@yahoo.com>
2020-11-11 16:11:46 +0300

    updated portfolio

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   get '/contact', to: 'pages#contact'
   get '/cocktail', to: 'pages#cocktail'
   get '/viewbot', to: 'pages#viewbot'
+  get '/resume', to: 'pages#resume'
+  get '/education', to: 'pages#education'
+  get '/project', to: 'pages#project'
   get '/superhuman', to: 'pages#superhuman'
+
 end
